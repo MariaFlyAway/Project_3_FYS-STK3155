ce690672192ec33f0f1bc193124ee15a369bb4b8
MariaFlyAway <mariakov@uio.no>
2024-12-03 19:06:24 +0100

    Moved evaluation and added neural net

---
+++ Code/Generation/neural_net.py
@@ -0,0 +1,18 @@
+import torch
+
+class NeuralNet(torch.nn.Module):
+
+    def __init__(self, input_dim=1024, output_dim=5):
+        super(NeuralNet, self).__init__()
+
+        self.linear1 = torch.nn.Linear(input_dim, 200)
+        self.activation = torch.nn.Sigmoid()
+        self.linear2 = torch.nn.Linear(200, output_dim)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        x = self.softmax(x)
+        return x
